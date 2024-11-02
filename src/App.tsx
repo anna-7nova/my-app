68e10fc0a2e1ccb4a75a665aca9b22b6c7df2983
anna-7nova <anna.7nova@yandex.by>
2024-11-02 20:58:34 +0300

    add the project to new repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import { Rating } from './components/Rating/Rating';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppTitle/>
+      <Rating value={5}/>
+      <Accordion titleValue="Menu" collapsed={false}/>
+      <Accordion titleValue="List" collapsed={true}/>
+      <Rating value={2}/>
     </div>
   );
 }
+
+function AppTitle() {
+  return (<>this is the new app</>);
+}
+
+
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log("accordion is rendering")
+    if (props.collapsed) {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue}/>
+                <AccordionBody />
+            </div>
+        );
+    }
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+        </div>
+    );
+    }
+    
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("title of accordion is rendered")
+        return <h4>--{props.title}--</h4>
+}
+
+function AccordionBody() {
+    console.log("body of accordion is rendered")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    );
+}
+
+export default Accordion;
